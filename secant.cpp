adc04a2bad074fce16c28835f05716803e6b44b4
Kai Rowlands <kaiwrowlands@gmail.com>
2026-01-19 16:35:38 -0500

    Add files via upload

---
+++ secant.cpp
@@ -0,0 +1,40 @@
+// Kai Rowlands
+// Modified Secant Method
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+// Define the function we are applying the modified secant method to
+double f(double x){
+    return x * x * x / 5.0 + sin(2.0 * x) - 2.0;
+}
+
+double delta = 0.01;    // Define the pertubation value
+
+// Define the approximation of the derivative of f
+double df(double x0){
+    return f(x0 + delta) - f(x0) / delta;
+}
+
+double secant(double x0){
+    double h = (delta * f(x0)) / (f(x0 + delta) - f(x0));
+    double tolerance = 0.00001;
+
+    while(abs(h) >= tolerance){
+        h = (delta * f(x0)) / (f(x0 + delta) - f(x0));
+        x0 = x0 - h;                                    // Modified secant method formula
+    }
+    cout << "The root of the f is: " << x0;
+
+    return x0;   // Returns the root of f
+}
+
+int main(){
+    double x0 = -1.0;   // Initial guess
+    
+    secant(x0);         // Computes the modified secant method evaluated at x0
+
+    return 0;
+}
